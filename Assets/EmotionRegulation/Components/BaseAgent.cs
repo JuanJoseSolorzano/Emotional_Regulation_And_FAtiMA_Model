34e0b1380b71a032b85f929ca2d7404ff7c21443
JuanJose <juanjose.solorzano.c@gmailcom>
2022-03-03 21:50:02 -0600

    Save

--- Assets/EmotionRegulation/Components/BaseAgent.cs
+++ Assets/EmotionRegulation/Components/BaseAgent.cs
@@ -4,7 +4,7 @@ using EmotionRegulation.Components;
 using RolePlayCharacter;
 using WellFormedNames;
 using ActionLibrary;
-
+using System.Diagnostics;
 
 namespace EmotionRegulation.BigFiveModel
 {
@@ -21,34 +21,42 @@ namespace EmotionRegulation.BigFiveModel
         public RequiredData RequiredData { get; set; }
 
         internal PersonalityDTO personality;
+        internal EmotionRegulationModel results;
         public BaseAgent(RolePlayCharacterAsset agentFAtiMA, PersonalityDTO personalityDTO, RequiredData info)
         {
- 
-            FAtiMACharacter = agentFAtiMA;
-            RequiredData = info;
+            if (personalityDTO is null)
+            {
+                throw new ArgumentNullException(nameof(personalityDTO));
+            }
+
+            FAtiMACharacter = agentFAtiMA ?? throw new ArgumentNullException(nameof(agentFAtiMA));
+            RequiredData = info ?? throw new ArgumentNullException(nameof(info));
             CreateAgente(personalityDTO);
 
         }
-        public BaseAgent() { }
 
         private void CreateAgente(PersonalityDTO personalityDTO)
         {
+            if(personalityDTO is null)
+                throw new ArgumentNullException(nameof(personalityDTO));
             if (RequiredData is null)
                 throw new ArgumentNullException(nameof(RequiredData));
-            else
-            {
-                AgentName = FAtiMACharacter.CharacterName;
-                var personality = new Personality(personalityDTO);
-                var BigFive = personality.BigFive;
-                DominantPersonality = BigFive.DominantPersonality;
-                StrategiesToApply = BigFive.StrategiesToApply;
-                AllPersonalities = BigFive.AllPersonalities;
-                StrategyMetrics = BigFive.StrategyMetrics;
-                this.personality = personalityDTO;
 
-            }
+            AgentName = FAtiMACharacter.CharacterName;
+            var personality = new Personality(personalityDTO);
+            var BigFive = personality.BigFive;
+            DominantPersonality = BigFive.DominantPersonality;
+            StrategiesToApply = BigFive.StrategiesToApply;
+            AllPersonalities = BigFive.AllPersonalities;
+            StrategyMetrics = BigFive.StrategyMetrics;
+            this.personality = personalityDTO;
         }
-
+        /// <summary>
+        /// Este método es el primer paso hacía la regualción de las emciones. Aquí se analizan todos los datos necesaríos 
+        /// para verificar si es posible llevar a cabo el proceso de regulación emocinal.
+        /// </summary>
+        /// <param name="decision">Posible dicisión que el agente intentará llevar a cabo dentro del escenario</param>
+        /// <returns></returns>
         public IAction Regulates(IAction decision)
         {
             if (decision is null)
@@ -57,19 +65,33 @@ namespace EmotionRegulation.BigFiveModel
                 throw new ArgumentException("The new agent is null", nameof(BaseAgent));
             if (FAtiMACharacter is null)
                 throw new ArgumentException("The character is null", nameof(FAtiMACharacter));
-            /// Paso 2.1.- Parte del segundo paso consiste en verificar si la posible decisión provocará una emoción negativa,
-            /// para eso es necesario calcular la emoción:
-            /// 
-            var data = new PreparingEmotionRegulationData(); 
-            // Cuando se inicializa éste método, se cargan los primeros datos, en este caso se carga las reglas de valoración
-            // del evento.
-            var negative = data.GetEmotions(FAtiMACharacter, decision);
 
-  
-            if (negative) 
+            ///-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-++-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+
+            /// Paso 2.1:
+            ///     Parte del segundo paso consiste en verificar si la posible decisión provocará una emoción negativa en el 
+            ///     agente.La clase LoadingEmotionalRegulation contiene un método que cácula las posibles emociones y otro que 
+            ///     verifica la existencia de datos suficientes(eventos que para ser evitados, acciones para la segunda 
+            ///     estrategia, etc.) para llevar a cabo la regulación emocional. A través del método ApplyEmotionRegulation se 
+            ///     revisa si se cumplen estas dos condiciones (que el evento provoque emociones negativas y que existan los 
+            ///     datos necesarios). Si se cumplen las condiciones el método retorna un valor verdadero y se crea un nuevo 
+            ///     objeto de tipo EmotionRegulationModel para aplicar las estrategias ahí definidas.
+            ///-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-++-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+
+
+            var data = new LoadingEmotionalRegulation(); 
+            
+            var applyRegulation = data.ApplyEmotionRegulation(this, decision);
+            
+            if (applyRegulation) 
             {
-                data.SetDataInformation(this, decision);
-                var results = new EmotionRegulationModel(decision, data);
+                ///-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-++-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+
+                /// Paso 3:
+                ///     Una vez se han verificado todos los datos, se procede a relizar la regualción de la emoción(s) causada
+                ///     por el evento. Se crea el objeto EmotionRegulationModel, pasando como parámetros la acción/evento/
+                ///     decision que probocará la emoción(s) negativa y el objeto creado data, el cual contiene los datos 
+                ///     necesarios para iniciar con la regulación.
+                ///-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-++-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+
+                results = new EmotionRegulationModel(decision, data);
+
                 return results.newAction;
             }
             else
